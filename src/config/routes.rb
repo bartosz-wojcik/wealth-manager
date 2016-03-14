60fc4bb6b91783021d12f227bea7554f381f0ea4
Bartosz Wójcik <bartek@procreative.eu>
2016-03-15 00:11:23 +0100

    users handling in progress

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup' => 'sessions#new', as: :signup
+  post 'signup' => 'sessions#save', as: :signup_save
+  get 'login' => 'sessions#create', as: :login
+  get 'logout' => 'sessions#destroy', as: :logout
+  post 'login' => 'sessions#create'
+
+  get 'accounts/details'
+  patch 'accounts/details'
+  get 'accounts/password'
+  patch 'accounts/password'
+  post 'accounts/password'
+  get 'accounts/resend_activation'
+
+  # account activation urls
+  get 'activate' => 'sessions#activate_start', as: :activate_start
+  post 'activate' => 'sessions#activate_start', as: :activate_start_post
+  get 'activate/:key' => 'sessions#activate', as: :activate
+
+  # password recovery urls
+  get 'recover' => 'sessions#recover_start', as: :recover_start
+  post 'recover' => 'sessions#recover_start', as: :recover_start_post
+  get 'recover/:key' => 'sessions#recover', as: :recover
+
+  root 'site#index'
 end
