4655223bca832353475aa2f8de8f8b84096f92bc
Bartosz Wójcik <bartek@procreative.eu>
2016-03-15 23:00:41 +0100

    some more updates to login/registration process

--- src/app/controllers/site_controller.rb
+++ src/app/controllers/site_controller.rb
@@ -1,8 +1,14 @@
 class SiteController < ApplicationController
 
   def index
-    # for now just redirect to login
-    redirect_to :login
+    self.page_title = 'Dashboard'
+
+    if current_account.is_a? Account
+      render 'dashboard'
+    else
+      # for now just redirect to login
+      redirect_to :login
+    end
   end
 
 end
