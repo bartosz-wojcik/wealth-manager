080b69b015a508c634382bbff0ba176f72c72ed2
Bartosz Wójcik <bartek@procreative.eu>
2016-03-19 01:20:16 +0100

    users management tests updated and improved

--- src/test/controllers/sessions_controller_test.rb
+++ src/test/controllers/sessions_controller_test.rb
@@ -9,22 +9,25 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_includes @response.body, 'Password'
   end
 
-  # test 'should login' do
-  #   user = accounts(:user)
-  #   post '/login', params: { email: user.email, password: user.password }
-  #   assert_redirected_to root_path
-  #   assert_not_nil flash[:success]
-  # end
+  test 'should login' do
+    user = accounts(:user)
+    post '/login', params: { email: user.email, password: 'user123' }
+    assert_redirected_to root_path
+    assert_not_nil session[:user_id]
+    assert_not_nil flash[:success]
+  end
 
-  # test 'should not login 1' do
-  #   post '/login', params: { email: 'user@example.com', password: 'invalid' }
-  #   assert_redirected_to login_path
-  #   assert_not_nil flash[:danger]
-  # end
+  test 'should not login 1' do
+    post '/login', params: { email: 'user@example.com', password: 'invalid' }
+    assert_redirected_to login_path
+    assert_nil session[:user_id]
+    assert_not_nil flash[:danger]
+  end
 
   test 'should not login 2' do
     post '/login', params: { email: 'invalid@example.com', password: 'invalid' }
     assert_redirected_to login_path
+    assert_nil session[:user_id]
     assert_not_nil flash[:danger]
   end
 
@@ -32,12 +35,22 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   test 'should logout' do
     get '/logout'
     assert_redirected_to login_url
+    assert_nil session[:user_id]
     assert_not_nil flash[:success]
   end
 
   test 'should_get_activate_start' do
     get '/activate'
     assert_response :success
+    assert_includes @response.body, 'Your email'
+    assert_includes @response.body, 'Start activation'
+  end
+
+  test 'should_get_recover_start' do
+    get '/recover'
+    assert_response :success
+    assert_includes @response.body, 'Your email'
+    assert_includes @response.body, 'Start recovery'
   end
 
 end
