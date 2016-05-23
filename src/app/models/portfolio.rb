3e854ced22951f64def09f1900ab71de46c8136e
Bartosz Wójcik <bartek@procreative.eu>
2016-05-23 09:05:00 +0200

    implementing portfolio historical values calculations in progress

--- src/app/models/portfolio.rb
+++ src/app/models/portfolio.rb
@@ -28,34 +28,20 @@ class Portfolio < ApplicationRecord
       currency = account.currency
     end
     full_values     = []
-    relative_to     = nil
-    current_value   = 0
     exclude_records = []
+    current_date    = nil
 
     loop do
-      current_record = next_value_record(nil, nil, relative_to, exclude_records)
-      break unless current_record.present?
+      relative_to  = prev_value_record(nil, current_date, exclude_records)
+      break unless relative_to.present?
 
-      exclude_records << current_record.id
-      if current_record.currency_id != currency.id
-        result_value = currency.convert_from(current_record.value, current_record.currency)
-        # it was not possible to convert currency
-        unless result_value.present?
-          result_value = current_record.value
-          # TODO: add handling of this situation
-          # @current_currency = full_value.currency
-        end
-      else
-        result_value = current_record.value
-      end
-
-      relative_to   = current_record
-      current_value = current_record.partial_value ?
-          current_value + result_value : result_value
-      full_values << { :date => relative_to.entered_date, :value => '%.2f' % current_value }
+      current_date = relative_to.entered_date
+      full_value   = final_value(currency, nil, current_date)
+      full_values     << { :date => current_date, :value => '%.2f' % full_value }
+      exclude_records << relative_to.id
     end
 
-    full_values
+    full_values.reverse
   end
 
   def last_month_change
@@ -63,9 +49,12 @@ class Portfolio < ApplicationRecord
   end
 
 
-  def final_value(currency, categories = nil)
+  def final_value(currency, categories = nil, for_date = nil)
     result_value = 0
-    records = value_records(categories)
+    if for_date.nil?
+      for_date = Date.today
+    end
+    records = value_records(for_date, categories)
     # go through all records...
     records.each do |category, values|
       values.each do |value|
@@ -98,57 +87,36 @@ class Portfolio < ApplicationRecord
   end
 
   private
-  def next_value_record(partial_value, categories = nil, relative_to = nil, exclude_records = [])
-    if categories.present?
-      change = PortfolioChange.joins(:portfolio).order('entered_date ASC, id ASC')
-      if partial_value == nil
-        change = change
-           .where('portfolio_id = ? AND asset_category_id IN (?) AND entered_date >= ?' +
-                    ' AND portfolio_changes.id NOT IN (?)',
-                  id, categories, relative_to.present? ? relative_to.entered_date : '1900-01-01',
-                  exclude_records
-           )
-      else
-        change = change
-           .where('portfolio_id = ? AND asset_category_id IN (?) AND partial_value = ? AND entered_date >= ?' +
-                    ' AND portfolio_changes.id NOT IN (?)',
-                  id, categories, partial_value ? true : false,
-                  relative_to.present? ? relative_to.entered_date : '1900-01-01',
-                  exclude_records
-           )
-      end
-    else
-      change = PortfolioChange.joins(:portfolio).order('entered_date ASC, id ASC')
-      if partial_value == nil
-        change = change
-           .where('portfolio_id = ? AND entered_date >= ?' +
-                    ' AND portfolio_changes.id NOT IN (?)',
-                  id, relative_to.present? ? relative_to.entered_date : '1900-01-01',
-                  exclude_records
-           )
-      else
-        change = change
-           .where('portfolio_id = ? AND partial_value = ? AND entered_date >= ?' +
-                    ' AND portfolio_changes.id NOT IN (?)',
-                  id, partial_value ? true : false, relative_to.present? ? relative_to.entered_date : '1900-01-01',
-                  exclude_records
-           )
-      end
+  def prev_value_record(categories = nil, relative_to = nil, exclude_records = [])
+    if categories.blank?
+      categories = all_categories.to_a
     end
+
+    change = PortfolioChange.order('entered_date DESC, id DESC')
+    if exclude_records.empty?
+      change = change.where('portfolio_id = ? AND asset_category_id IN (?) AND entered_date <= ?',
+                            id, categories, relative_to.present? ? relative_to : Date.today)
+    else
+      change = change.where('portfolio_id = ? AND asset_category_id IN (?) AND entered_date <= ?' +
+                                ' AND id NOT IN (?)',
+                            id, categories, relative_to.present? ? relative_to : Date.today,
+                            exclude_records)
+    end
+
     change.first
   end
 
-  def value_records(categories = nil)
+  def value_records(for_date, categories = nil)
     records = {}
     if categories.blank?
-      categories = self.all_categories.to_a
+      categories = all_categories.to_a
     end
     # gets all portfolio changes grouped by category
     categories.each do |c|
       records[c] = PortfolioChange
-                    .joins(:portfolio)
-                    .where('portfolio_id = ? AND asset_category_id = ?', id, c)
-                    .order('entered_date DESC')
+                    .where('portfolio_id = ? AND asset_category_id = ? AND entered_date <= ?',
+                           id, c, for_date)
+                    .order('entered_date DESC, id DESC')
                     .all.to_a
     end
     records
