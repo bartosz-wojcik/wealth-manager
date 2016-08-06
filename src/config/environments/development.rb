7162f72476bb761404f18b1c7f31a6eb447fa691
Bartosz Wójcik <bartek@procreative.eu>
2016-08-06 14:41:57 +0200

    using dotenv gem for managing custom environment configuration

--- src/config/environments/development.rb
+++ src/config/environments/development.rb
@@ -31,6 +31,16 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV['MAIL_USERNAME'],
+      :password => ENV['MAIL_PASSWORD'],
+      :address => ENV['MAIL_ADDRESS'],
+      :domain => ENV['MAIL_DOMAIN'],
+      :port => ENV['MAIL_PORT'],
+      :authentication => :cram_md5
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -45,7 +55,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  Rails.application.routes.default_url_options[:host] = ENV['HOST_URL']
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
